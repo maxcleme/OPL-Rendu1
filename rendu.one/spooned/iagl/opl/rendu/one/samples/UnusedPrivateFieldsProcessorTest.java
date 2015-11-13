32201a22a7b622fcdc9c4d6d13d574ef9b434a6f
jpiorun <jpiorun@unis.fr>
2015-11-13 12:42:37 +0100

    jgf

---
+++ rendu.one/spooned/iagl/opl/rendu/one/samples/UnusedPrivateFieldsProcessorTest.java
@@ -0,0 +1,33 @@
+package iagl.opl.rendu.one.samples;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnusedPrivateFieldsProcessorTest {
+    private int b;
+
+    private int a;
+
+    @Test
+    public void test() {
+        b = 2;
+        Assert.fail("Not yet implemented");
+    }
+
+    public int getB() {
+        return this.b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return this.a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
+
